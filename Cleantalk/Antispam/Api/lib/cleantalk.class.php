b7f2448ef9bde4cf8995a145c16e88bdb6cfb77d
davydovct <davydov@cleantalk.org>
2018-03-16 15:08:56 +0500

    Fix get_ip outside of class

--- Cleantalk/Antispam/Api/lib/cleantalk.class.php
+++ Cleantalk/Antispam/Api/lib/cleantalk.class.php
@@ -306,7 +306,8 @@ class CleantalkRequest {
 /**
  * Cleantalk class create request
  */
-class Cleantalk {
+class Cleantalk 
+{
 
     /**
      * Debug level
@@ -791,7 +792,38 @@ class Cleantalk {
 
         return $response;
     }
+    /**
+     * Function to get the user IP
+     */    
+    public function cleantalk_get_real_ip()
+    {
+      // Getting headers
+      $headers = function_exists('apache_request_headers') ? apache_request_headers() : $_SERVER;
 
+      // Getting IP for validating
+      if (array_key_exists( 'X-Forwarded-For', $headers )){
+        $ip = explode(",", trim($headers['X-Forwarded-For']));
+        $ip = trim($ip[0]);
+      }elseif(array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers)){
+        $ip = explode(",", trim($headers['HTTP_X_FORWARDED_FOR']));
+        $ip = trim($ip[0]);
+      }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+      }
+
+        // Validating IP
+        // IPv4
+      if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)){
+        $the_ip = $ip;
+        // IPv6
+      }elseif(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)){
+        $the_ip = $ip;
+        // Unknown
+      }else{
+        $the_ip = null;
+      }
+      return $the_ip;
+    } 
     /**
      * Function to get the message hash from Cleantalk.ru comment
      * @param $message
@@ -946,37 +978,10 @@ class Cleantalk {
         
         return $str;
     }
+
 }
 
-    public function cleantalk_get_real_ip()
-    {
-      // Getting headers
-      $headers = function_exists('apache_request_headers') ? apache_request_headers() : $_SERVER;
 
-      // Getting IP for validating
-      if (array_key_exists( 'X-Forwarded-For', $headers )){
-        $ip = explode(",", trim($headers['X-Forwarded-For']));
-        $ip = trim($ip[0]);
-      }elseif(array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers)){
-        $ip = explode(",", trim($headers['HTTP_X_FORWARDED_FOR']));
-        $ip = trim($ip[0]);
-      }else{
-        $ip = $_SERVER['REMOTE_ADDR'];
-      }
-
-        // Validating IP
-        // IPv4
-      if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)){
-        $the_ip = $ip;
-        // IPv6
-      }elseif(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)){
-        $the_ip = $ip;
-        // Unknown
-      }else{
-        $the_ip = null;
-      }
-      return $the_ip;
-    }
 /**
  * Function gets access key automatically
  *
