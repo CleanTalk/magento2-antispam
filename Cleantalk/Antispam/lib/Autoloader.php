d09d84a0755b1ecf7cb48211a3e7b045e71a3b7c
Glomberg <bazz@bk.ru>
2025-07-08 20:12:46 +0300

    Fix. Code. Autoloader fixed.

---
+++ Cleantalk/Antispam/lib/Autoloader.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cleantalk\Antispam\lib;
+
+/**
+ * Autoloader for \Cleantalk\* classes
+ *
+ * @param string $class
+ *
+ * @return void
+ */
+class Autoloader
+{
+    public static function autoload()
+    {
+        spl_autoload_register(function ($class) {
+            // Register class auto loader
+            // Custom modules1
+            if ( strpos($class, 'Cleantalk') !== false ) {
+                $class      = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+                $class_file = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
+                if ( file_exists($class_file) ) {
+                    require_once($class_file);
+                }
+            }
+        });
+    }
+}
--- Cleantalk/Antispam/registration.php
+++ Cleantalk/Antispam/registration.php
@@ -4,4 +4,4 @@
     'Cleantalk_Antispam',
     __DIR__
 );
-require_once __DIR__ . '/lib/autoloader.php';
+\Cleantalk\Antispam\lib\Autoloader::autoload();
